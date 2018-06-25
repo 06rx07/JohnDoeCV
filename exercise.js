746dedc982010b73169d7d8bc32b307a51251d07
Rachel Xi <rachelxi@Rachels-MacBook-Pro.local>
2018-06-26 00:45:40 +1200

    Add Greetings

--- exercise.js
+++ exercise.js
@@ -6,3 +6,18 @@ function toggleHobby() {
         getHobby.classList.add('visible');
     }
 }
+function showGreetings(){
+    const currentHour = (new Date()).getHours();
+    const greeting = document.getElementById("greetings");
+    let greetMsg = "";
+    if(currentHour >= 22 || currentHour < 6){
+        greetMsg = "Good Night!";
+    } else if(currentHour >= 6 && currentHour < 12){
+        greetMsg = "Good Morning!";
+    } else if(currentHour >= 12 && currentHour < 18){
+        greetMsg = "Good Afternoon!";
+    } else {
+        greetMsg = "Good Evening!";
+    }
+    greeting.innerHTML = greetMsg;
+}
